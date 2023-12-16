94aad3e0529220609b26011a348f1e85ada34dcb
Isidora Rojas <isidora.rojas.a@usach.cl>
2023-12-16 13:34:45 -0300

    Se crean carpetas de micro servicios

---
+++ Prestamo/src/test/java/com/example/Prestamo/PrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
